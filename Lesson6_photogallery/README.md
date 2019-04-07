dca22253f3b5d2f06c11af4c4bd87668497a1c48
Breanna Bush <brb14d@my.fsu.edu>
2019-04-06 23:54:22 -0400

    CP Update

---
+++ Lesson6_photogallery/README.md
@@ -0,0 +1,22 @@
+## Project Name:  Photo Gallery Application
+
+### Course Title:
+Web Application Development
+
+### Assignment Date:  
+(Month Day, Year)
+
+### Student Name:  
+(First and Last Name)
+
+### Project Description:
+(In your own words, briefly describe the assignment.)
+
+### View Project:
+(Replace this statement with your Github Page URL that was created when you 
+ published the project.)
+
+### Lessons Learned in the Assignment:
+1. (Briefly describe a lesson/concept learned in this lesson.)
+2. (Briefly describe a lesson/concept learned in this lesson.)
+3. (Briefly describe a lesson/concept learned in this lesson.)
---
+++ Lesson6_photogallery/photos.js
@@ -0,0 +1,110 @@
+/*    
+    Program Name:  Photo Gallery Application
+    Author: 
+    Date:   
+    Filename: photos.js
+    
+ */
+
+
+"use strict"; // interpret document contents in JavaScript strict mode
+
+/* global variables */
+var photoOrder = [1,2,3,4,5];
+var figureCount = 3;
+
+/*****************************************populateFigures() FUNCTION*************************************/
+/* This method adds src values to img elements based on order specified in photoOrder array             */
+/********************************************************************************************************/
+function populateFigures() {
+
+
+
+
+}//end of populateFigures function
+
+/**********************************END OF populateFigures() FUNCTION*************************************/
+
+
+
+/*********************************rightArrow() FUNCTION***************************************************/
+/* This method shifts all images one figure to the right, and change values in photoOrder array to match */
+/********************************************************************************************************/
+function rightArrow() {
+   for (var i = 0; i < 5; i++) {
+      if ((photoOrder[i] + 1) === 6) {
+         photoOrder[i] = 1;
+      } else {
+         photoOrder[i] += 1;
+      }//end of else
+      
+      populateFigures();
+      
+   }//end of for loop
+}//end of rightArrow Function
+
+/***********************************END OF RIGHT ARROW FUNCTION******************************************/
+
+/********************************leftArrow() Function****************************************************/
+/*This method shifts all images one figure to the left, and change values in photoOrder array to match  */
+/********************************************************************************************************/
+function leftArrow() {
+    for (var i = 0; i < 5; i++) {
+        if ((photoOrder[i] - 1) === 0) {
+            photoOrder[i] = 5;
+        } else {
+            photoOrder[i] -= 1;
+        }//end of else
+        
+      populateFigures();
+      
+    }//end of for loop
+}//end of leftArrow Function
+
+/****************************END OF LEFT ARROW FUNCTION*************************************************/
+
+
+/***************************************previewFive() Function******************************************/
+/*           This method is executed to allow the photo gallery to shift to five image layout.         */
+/******************************************************************************************************/
+function previewFive() {
+   
+
+   
+   
+}//end of previewFive()
+
+/****************************************END of previewFive() Function**********************************/
+
+
+
+/***********************************createEventListeners() Function*************************************/
+/*         This method attaches event listeners to the buttons (left, right, and show all).            */
+/*******************************************************************************************************/
+function createEventListeners() {
+   
+
+
+   
+}//end of createEventListeners function
+
+/************************************END OF createEventListeners() FUNCTION****************************/
+
+
+/************************************setUpPage() FUNCTION**********************************************/
+/* create event listeners and populate image elements */
+function setUpPage() {
+   createEventListeners();
+   populateFigures();
+}//end of setUpPage Function
+
+/***********************************END OF setUpPage() FUNCTION***************************************/
+
+
+
+/* run setUpPage() function when page finishes loading */
+if (window.addEventListener) {
+      window.addEventListener("load", setUpPage, false); 
+} else if (window.attachEvent)  {
+      window.attachEvent("onload", setUpPage);
+}//end of else if
