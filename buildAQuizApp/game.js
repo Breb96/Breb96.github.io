9d5bf890e2b00face76fbfc199fcc7508abcbed4
Breanna Bush <BreBush96@gmail.com>
2021-08-05 08:58:21 -0400

    Websites update
    I updated my professional experience on my personal website.
    I am still completing udemy courses.

--- buildAQuizApp/game.js
+++ buildAQuizApp/game.js
@@ -1,5 +1,8 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -40,8 +43,8 @@ const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
-    score =0;
-    availableQuestions = [...questions]
+    score = 0;
+    availableQuestions = [...questions];
     getNewQuestion();
 };
 
@@ -49,25 +52,28 @@ getNewQuestion = () => {
 
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
         //go to the end page
-        return window.location.assign("/end.html");
+        return window.location.assign('/end.html');
     }
     questionCounter++;
+    progressText.innerText = "Question" + questionCounter + "/" + MAX_QUESTIONS;
+        //update the progress bar
+        progressBarFull.style.width = '${(questionCounter / MAX_QUESTIONS) * 100}%';
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
         currentQuestion = availableQuestions[questionIndex];
-        question.innerText = currentQuestion.question;
+        question.innerHTML = currentQuestion.question;
 
-        choices.forEach ( choice => {
-            const number = choice.dataset['number'];
-            choice.innerText = currentQuestion['choice' + number];
-            }
-        );
+    choices.forEach (( choice ) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
 
-        availableQuestions.splice(questionIndex, 1);
-        acceptingAnswers = true;
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
 };
 
-choices.forEach( choice => {
-    choice.addEventListener("click", e => {
+choices.forEach(( choice ) => {
+    choice.addEventListener('click', (e) => {
         if(!acceptingAnswers) return;
 
         acceptingAnswers = false;
@@ -79,13 +85,22 @@ choices.forEach( choice => {
                 classToApply = 'correct';
             }
         
+            if(classToApply === 'correct'){
+                incrementScore(CORRECT_BONUS);
+            }
+
         selectedChoice.parentElement.classList.add(classToApply);
-        setTimeout( () => {
+        
+        setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
             getNewQuestion();
         }, 1000);
-        
     });
 });
+
+incrementScore = (num) => {
+    score += num;
+    scoreText.innerText = score;
+};
 
 startGame();
